eafed6f33767f27a4c28f292605491f8f10cb4a7
omah-hub <orjichioma422@yahoo.com>
2022-10-13 14:47:55 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main- Serves as the entry point for the program
+ *main- Serves as entry point for program
  *
  * Return: By default returns Zero if no erros otherwise returns non-zero value.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
