7198938f950b8e081ca1961c43107f2de9354794
omah-hub <orjichioma422@yahoo.com>
2022-10-13 15:05:39 -0700

    my commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return:0 if no errors,return non zero if errors
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
