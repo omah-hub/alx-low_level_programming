61b0fa4ade661309f74a50b74b37c9aa70f7549a
omah-hub <orjichioma422@yahoo.com>
2022-10-14 14:52:27 -0700

    my commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return:0 if successful
+ */
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j)
+	}
+	putchar('\n');
+	return (0);
+}
