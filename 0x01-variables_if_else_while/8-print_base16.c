fe65b9a25fa7e03223fb86c1be077c0b37e08c32
omah-hub <orjichioma422@yahoo.com>
2022-10-14 16:00:18 -0700

    my commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ *
+ * Return:0 if successful
+ */
+int main(void)
+{
+	int i;
+	char hexvalues[] = "0123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
