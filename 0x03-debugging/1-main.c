3f2b7a6f071bc109afe9fbaaeda4ecc9079dcecc
omah-hub <orjichioma422@yahoo.com>
2022-10-20 20:44:17 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
+ * main - causes an infinit loop
  * Return: 0
  */
 
@@ -10,15 +10,12 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-
-	/*while (i < 10 )*/
+	/*while (i < 10)*/
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
