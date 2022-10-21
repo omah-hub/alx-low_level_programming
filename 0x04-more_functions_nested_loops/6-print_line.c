472de06e2407711c0403270cd4e6e4b8484e321e
omah-hub <orjichioma422@yahoo.com>
2022-10-21 06:49:51 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * print_line - draws a straight line
+ * @n: number of times the character _ should be printed
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putcahr('_');
+		}
+		_putchar('\n');
+	}
+}
