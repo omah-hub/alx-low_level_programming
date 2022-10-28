70fc91c2073a182b09da3678222a650e41b0170f
omah-hub <orjichioma422@yahoo.com>
2022-10-27 20:43:08 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * rot13 - a function that encodes a string using rot13
+ * @s: An input string
+ * Return: An encode string
+ */
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'z') ||
+					(s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'a') ||
+					(s[i] >= 'A' && s[i] <= 'M'))
+				s[i] += 13;
+			else
+				s[i] -= 13;
+			i++;
+		}
+		i++;
+	}
+	retutn (s);
+}
