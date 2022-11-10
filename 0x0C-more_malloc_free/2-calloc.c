1683caf6da8c12f57b18cbb92db47642cd201782
omah-hub <orjichioma422@yahoo.com>
2022-11-10 13:55:51 -0800

    my commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 	if (p == NULL)
 	{
-		return (NULL)
+		return (NULL);
 	}
 	for (i = 0; i < (nmemb * size); i++)
 	{
