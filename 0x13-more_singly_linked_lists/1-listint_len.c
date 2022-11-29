321127acaefe95ca6fe662ff4bfb7c73faba9702
omah-hub <orjichioma422@yahoo.com>
2022-11-29 08:18:01 -0800

    my commit

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - calculates the number of element
+ * @h: Pointer to a list
+ * Return: Integer
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *tp;
+	unsigned int cnr = 0
+
+		tp = h;
+	while (tp)
+	{
+		cnr++;
+		tp = tp->next;
+	}
+	return (cnr);
+}
