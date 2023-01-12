38159445068f9029c38eadbb558c36a46a53e75d
omah-hub <orjichioma422@yahoo.com>
2023-01-12 13:37:39 -0800

    my commit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * add_dnodeint - Adds a new node at the beginning
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end
+ *
+ * @head: struct
+ *
+ * @n: const int
+ *
+ * Return: slistint_t
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *list = NULL, *end = *head;
+
+	list = malloc(sizeof(dlistint_t));
+
+	if (list == NULL)
+	{
+		list->n = n;
+		list->prev = NULL;
+		list->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head = list;
+			return (*head);
+		}
+
+		while (end->next != NULL)
+		{
+			end = end->next;
+		}
+		list->prev = end;
+		end->next = list;
+
+		return (list);
+	}
