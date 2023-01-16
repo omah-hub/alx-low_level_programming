64c128f06436889f658730f4ccd072704e95db64
omah-hub <orjichioma422@yahoo.com>
2023-01-16 13:00:39 -0800

    my commit

---
+++ 0x18-dynamic_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character tp print
+ * Return: On success 1
+ * On error, -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
