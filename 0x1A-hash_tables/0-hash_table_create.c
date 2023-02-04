750cca46128a359a06206dfce249262fdc8497bc
omah-hub <orjichioma422@yahoo.com>
2023-02-03 19:58:30 -0800

    task done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
 	unsigned long int i;
-	
+
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
@@ -19,8 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
-		return (NULL);													
-	}	
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
 	return (hash_table);
